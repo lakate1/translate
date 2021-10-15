8149bf3847d093c56b861b3b482de88e6371310b
Leah Queen <leah836@gmail.com>
2021-10-15 13:57:23 -0600

    Created the functionality to make the translation buttons work

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,25 @@
 import React from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
 
 class App extends React.Component {
-    state = { language: "english"};
+    state = { language: "english" };
 
     onLanguageChange = language => {
         this.setState({ language });
-    }
+    };
     
     render() {
         return (
-            <div ClassName="ui container">
+            <div className="ui container">
                 <div>
                     Select a language:
-                    <i className="flag us" onClick={() => this.onLanguageChange("English")} />
-                    <i className="flag nl" onClick={() => this.onLanguageChange("Dutch")} />
+                    <i className="flag us" onClick={() => this.onLanguageChange("english")} />
+                    <i className="flag nl" onClick={() => this.onLanguageChange("dutch")} />
                 </div>
-                <UserCreate /> 
+                <LanguageContext.Provider value={this.state.language}>
+                    <UserCreate />
+                </LanguageContext.Provider>
             </div>
         );
     }
---
+++ src/contexts/LanguageContext.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+// Sets the default language to english
+export default React.createContext("english");
