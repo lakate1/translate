fc49d60f18f4ff75d55c19dc48369399d696ab84
Leah Queen <leah836@gmail.com>
2021-10-28 10:16:14 -0600

    Hooked up the Language context

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,4 +1,25 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 // Sets the default language to english
-export default React.createContext("english");
+const Context = React.createContext("english");
+
+export class LanguageStore extends React.Component {
+    static contextType = LanguageContext;
+    
+    state = { language: "english "};
+
+    onLanguageChange = ( language ) => {
+        this.setState ({ language });
+    }
+
+    render() {
+        return (
+            <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }} >
+                {this.props.children}
+            </Context.Provider>
+        );
+    }
+}
+
+export default Context;
